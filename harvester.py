fc6b67eb48ebe2be045b03f3ac0f3c434faa8cb0
Alan Schoen <alan.schoen@gmail.com>
2016-09-01 14:40:19 -0400

    organize harvester

--- harvester.py
+++ harvester.py
@@ -1,7 +1,38 @@
 import tweepy
 
-# Helper Functions:
+# TwitterUser class has methods to read and store the activity of a twitter user
+class TwitterUser:
+    def __init__(self, screen_name, api, db):
+        self.screen_name = screen_name
+        self.db = db
+        self.api = api
+        self.check_tweetsat()
+        self.check_tweets()
 
+    def check_tweetsat(self):
+        collection_name = self.screen_name[1:] + '_tweetsat'
+        self.last_tweetsat = get_db_last(self.db, collection_name)
+
+    def check_tweets(self):
+        collection_name = self.screen_name[1:] + '_tweets'
+        self.last_tweets = get_db_last(self.db, collection_name)
+
+    def update_tweetsat(self, max_tweets, since_id=None):
+        collection_name = self.screen_name[1:] + '_tweetsat'
+        cursor = tweepy.Cursor(self.api.search, q=self.screen_name, count=300, since_id=self.last_tweetsat).items(max_tweets)
+        count = store_tweets(cursor, self.db, collection_name)
+        self.check_tweetsat()
+        print "stored %d tweets at %s" % (count, self.screen_name)
+
+    def update_tweets(self, max_tweets, since_id=None):
+        collection_name = self.screen_name[1:] + '_tweets'
+        cursor = tweepy.Cursor(self.api.user_timeline, screen_name="@donlemon", count=300, since_id=self.last_tweets).items(
+            max_tweets)
+        count = store_tweets(cursor, self.db, collection_name)
+        self.check_tweets()
+        print "stored %d of %s's tweets" % (count, self.screen_name)
+
+# Helper Functions:
 # Enforce time api limits
 def limit_handled(cursor):
     while True:
@@ -44,36 +75,3 @@ def get_db_last(db, collection_name):
         return db[collection_name].find().sort([("id", -1)]).limit(1).next()['id']
     else:
         return None
-
-
-# TwitterUser class has methods to read and store the activity of a twitter user
-class TwitterUser:
-    def __init__(self, screen_name, api, db):
-        self.screen_name = screen_name
-        self.db = db
-        self.api = api
-        self.check_tweetsat()
-        self.check_tweets()
-
-    def check_tweetsat(self):
-        collection_name = self.screen_name[1:] + '_tweetsat'
-        self.last_tweetsat = get_db_last(self.db, collection_name)
-
-    def check_tweets(self):
-        collection_name = self.screen_name[1:] + '_tweets'
-        self.last_tweets = get_db_last(self.db, collection_name)
-
-    def update_tweetsat(self, max_tweets, since_id=None):
-        collection_name = self.screen_name[1:] + '_tweetsat'
-        cursor = tweepy.Cursor(self.api.search, q=self.screen_name, count=300, since_id=self.last_tweetsat).items(max_tweets)
-        count = store_tweets(cursor, self.db, collection_name)
-        self.check_tweetsat()
-        print "stored %d tweets at %s" % (count, self.screen_name)
-
-    def update_tweets(self, max_tweets, since_id=None):
-        collection_name = self.screen_name[1:] + '_tweets'
-        cursor = tweepy.Cursor(self.api.user_timeline, screen_name="@donlemon", count=300, since_id=self.last_tweets).items(
-            max_tweets)
-        count = store_tweets(cursor, self.db, collection_name)
-        self.check_tweets()
-        print "stored %d of %s's tweets" % (count, self.screen_name)

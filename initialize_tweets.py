ff310c8d86f333aa58f2f6dc602ebe5cd58df614
Alan Schoen <alan.schoen@gmail.com>
2016-09-01 14:40:05 -0400

    add initialization script

---
+++ initialize_tweets.py
@@ -0,0 +1,30 @@
+# Set up twitter access
+# pip install tweepy
+import cnfg
+import tweepy
+config = cnfg.load(".twitter_config")
+auth = tweepy.OAuthHandler(config["consumer_key"],
+                           config["consumer_secret"])
+auth.set_access_token(config["access_token"],
+                      config["access_token_secret"])
+api=tweepy.API(auth)
+
+# Set up database
+from pymongo import MongoClient
+client = MongoClient()
+db = client.retweets
+
+# Set max tweets for initialization and update
+max_tweets_init=3000
+
+accounts = ['@donlemon', '@kanyewest', '@realDonaldTrump', '@JusticeWillett', '@IAmSteveHarvey', '@juliaioffe',
+            '@ForecasterEnten', '@pmarca']
+
+from harvester import TwitterUser
+for username in accounts:
+    user = TwitterUser(username, api, db)
+    user.update_tweetsat(max_tweets_init)
+    user.update_tweets(max_tweets_init)
+print 'Done updating'
+
+

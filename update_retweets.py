daeb54bd673057c526b5a5d4e7ffab848da8d82b
Alan Schoen <alan.schoen@gmail.com>
2016-09-03 16:31:49 -0400

    add users to update_retweets

--- update_retweets.py
+++ update_retweets.py
@@ -18,7 +18,7 @@ db = client.retweets
 
 
 accounts = ['@donlemon', '@kanyewest', '@realDonaldTrump', '@JusticeWillett', '@IAmSteveHarvey', '@juliaioffe',
-            '@ForecasterEnten', '@pmarca']
+            '@ForecasterEnten', '@pmarca', '@wikileaks', '@joerogan', '@BenedictEvans', '@pescami']
 
 for user in accounts:
     tweets_collection = db[user[1:] + '_tweets']
